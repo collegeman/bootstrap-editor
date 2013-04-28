b0336141b56ecfd52ff65e6e9e02c62512a175a0
Aaron Collegeman <aaron@collegeman.net>
2013-04-28 15:12:29 -0400

    Put plupload in dist

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ cp js/bootstrap-editor.js ./dist/js
 cp css/* ./dist/css
 cp font/* ./dist/font
 cp -R docs/js/tinymce ./dist/js
+cp -R docs/js/plupload ./dist/js
 rm -fr ./dist/js/tinymce/.git*
 yuicompressor --preserve-semi ./dist/js/bootstrap-editor.js > ./dist/js/bootstrap-editor.min.js
 yuicompressor --preserve-semi ./dist/css/bootstrap-editor.css > ./dist/css/bootstrap-editor.min.css
